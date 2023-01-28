4cf71f73b055f4b5472e6625c62894faa4503c56
Caio Borges <caiowagnerborges@gmail.com>
2023-01-27 23:25:24 -0300

    new commit

---
+++ teste.js
@@ -0,0 +1,21 @@
+const cidade = 
+[
+    {
+    imagem : "./imagens/mage.jpg",
+    nome : "Magé",
+    texto : "Uma cidade pica, menor. Cheia de mato e gente burra. As vezes, acredita-se ser possível encontrar o Nilton."
+    },
+
+    {
+    imagem : "./imagens/sg.jpg",
+    nome : "São Gonçalo",
+    texto : "Cidade do vapo. Moscou, roubou. Olhos abertos, ou até sua virgindade vão roubar."
+    },
+
+    {
+    imagem : "./imagens/cpx_penha.jpg",
+    nome : "cpmplexo da Penha",
+    texto : "*Sons de tiro* papapa poupoupou, bala quebrando janela, cervejinha torando, cheirada no pó e mais tiro."
+    }
+]
+
